cd65f3cb20c73781dabe53c78392f6a51f16e519
rahulkul7218 <kulkarnirahul134@gmail.com>
2025-10-03 12:09:38 +0530

    set bydefault Delivery terms value on sales order and invoice and also bydefault set Packing and Insurance on sales invoice

--- renu_customization/hooks.py
+++ renu_customization/hooks.py
@@ -342,10 +342,12 @@ doctype_js = {
     "public/js/fetched_warrantydetails_on_deliverynoteitem.js", "public/js/fetched_warranty_date_from_delievrynote_if_date_of_dispatch.js"],
     # "public/js/calculate_end_date_on_delivery_note_and_update_serial_no.js"
     "Sales Invoice":["public/js/if_invoice_type_have_data_then_fetched_stored_in_invoice_typ_field.js",
-    "public/js/fetched_warrantydate_on_salesinvoiceitem_if_date_of_invoice.js","public/js/calculate_package_wait.js", "public/js/calculate_warranty_end_date.js", "public/js/update_serial_no_on_sales_invoice_while_submitting.js"],
+    "public/js/fetched_warrantydate_on_salesinvoiceitem_if_date_of_invoice.js","public/js/calculate_package_wait.js", "public/js/calculate_warranty_end_date.js", 
+    "public/js/update_serial_no_on_sales_invoice_while_submitting.js", "public/js/set_by_default_value_of_delivery_terms_on_sales_invoice.js"],
+
     "Purchase Invoice":"public/js/unmandatory_supplier_invoice_no_on_purchaseinvoice.js",
     
-    "Sales Order": "public/js/fetched_warranty_on_selecteditem_and_warrantdays_warrantybegins.js",
+    "Sales Order": ["public/js/fetched_warranty_on_selecteditem_and_warrantdays_warrantybegins.js","public/js/set_bydefalut_value_of_deliveryterms_packing_and_insurance_on_sales_order.js"],
     "Pick List": "public.js/fetched_warranty_details_on_salesorder_picklistitem.js",
     "Serial No": ["public/js/hide_warranty_field_onserial_no.js","public/js/make_warranty_expiry_date_read_only.js"]
      
---
+++ renu_customization/public/js/set_bydefalut_value_of_deliveryterms_packing_and_insurance_on_sales_order.js
@@ -0,0 +1,10 @@
+frappe.ui.form.on("Sales Order", {   // Replace "Sales Order" with your Doctype name
+    onload: function(frm) {
+        if (!frm.doc.delivery_term) {
+            frm.set_value("delivery_term", "EX-Works Pune");
+            frm.set_value("packing", "Included in unit price");
+            frm.set_value("insurance", "To Your account");
+            
+        }
+    }
+});
