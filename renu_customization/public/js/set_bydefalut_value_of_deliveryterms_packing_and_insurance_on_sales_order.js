c3fea1757f3ec240dbfb2e6ead23880a1b970c21
rahulkul7218 <kulkarnirahul134@gmail.com>
2025-10-06 13:28:42 +0530

    update and rename delivery term

--- renu_customization/public/js/set_bydefalut_value_of_deliveryterms_packing_and_insurance_on_sales_order.js
+++ renu_customization/public/js/set_bydefalut_value_of_deliveryterms_packing_and_insurance_on_sales_order.js
@@ -1,7 +1,7 @@
 frappe.ui.form.on("Sales Order", {   // Replace "Sales Order" with your Doctype name
     onload: function(frm) {
         if (!frm.doc.delivery_term) {
-            frm.set_value("delivery_term", "EX-Works Pune");
+            frm.set_value("delivery_term", "Ex-works Pune, India");
             frm.set_value("packing", "Included in unit price");
             frm.set_value("insurance", "To Your account");
             
