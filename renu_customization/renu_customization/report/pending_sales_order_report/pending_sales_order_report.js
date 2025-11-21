6679aece0da05aeeb20ff3012e249612d33d3a96
rahulkul7218 <kulkarnirahul134@gmail.com>
2025-11-21 16:44:25 +0530

    changes update in pending sales order report done

---
+++ renu_customization/renu_customization/report/pending_sales_order_report/pending_sales_order_report.js
@@ -0,0 +1,49 @@
+// Copyright (c) 2025, Assimilate Technologies Pvt Ltd and contributors
+// For license information, please see license.txt
+
+
+
+ 
+frappe.query_reports["Pending Sales Order Report"] = {
+    filters: [
+        {
+            fieldname: "creation_no",
+            label: "SO No",
+            fieldtype: "Link",
+            options: "Sales Order",
+            reqd: 0
+        },
+        {
+            fieldname: "from_date",
+            label: "From SO Date",
+            fieldtype: "Date",
+        },
+        {
+            fieldname: "to_date",
+            label: "To SO Date",
+            fieldtype: "Date",
+        },
+       
+        {
+            fieldname: "customer_name",
+            label: "Customer Name",
+              fieldtype: "Link",
+            options: "Customer"
+        },
+       {
+    fieldname: "item_code",
+    label: "Item Code",
+    fieldtype: "Link",
+    options: "Item"
+},
+        {
+            fieldname: "currency",
+            label: "Currency",
+            fieldtype: "Link",
+            options: "Currency"
+        }
+    ]
+};
+ 
+ 
+ 
---
+++ renu_customization/renu_customization/report/pending_sales_order_report/pending_sales_order_report.py
@@ -0,0 +1,217 @@
+# Copyright (c) 2025, Assimilate Technologies Pvt Ltd and contributors
+# For license information, please see license.txt
+
+import frappe
+ 
+from frappe import _
+ 
+def execute(filters=None):
+ 
+    columns = get_columns()
+ 
+    data = get_data(filters)
+ 
+    return columns, data
+ 
+ 
+def get_columns():
+ 
+    return [
+ 
+        _("SO No") + ":Link/Sales Order:150",
+ 
+        _("SO Date") + ":Date:120",
+ 
+        _("Sr.No.") + ":Int:70",
+ 
+        _("Customer's PO No.") + ":Data:170",
+ 
+        _("Customer's PO Date") + ":Date:170",
+ 
+        _("Customer Code") + ":Link/Customer:150",
+ 
+        _("Customer Name") + ":Data:180",
+ 
+        _("Party Item Code") + ":Link/Item:150",
+ 
+        _("Item Code") + ":Link/Item:120",
+ 
+        _("Item Name") + ":Data:180",
+ 
+        _("Description") + ":Data:250",
+ 
+        _("Order Quantity") + ":Float:120",
+ 
+        _("Delivered Qty") + ":Float:120",
+ 
+        _("Open Qty") + ":Float:120",
+ 
+       
+ 
+        _("Item Rate") + ":Float:120",
+ 
+        _("Currency") + ":Link/Currency:100",
+ 
+        _("Exchange Rate") + ":Float:150",
+ 
+        # _("PO Total") + ":Float:120",
+ 
+        _("Total Net Amount (INR)") + ":Float:180",
+ 
+         _("Delivered Net Total") + ":Float:200",
+ 
+         _("Balance Net Total") + ":Float:180",
+ 
+        # _("Invoice Grand Total") + ":Float:180",
+ 
+        # _("Outstanding Amount") + ":Float:170",
+ 
+        _("Delivery Date") + ":Date:120",
+ 
+        _("Stock") + ":Float:150",
+ 
+        _("Business Region Name") + ":Data:190",
+ 
+        _("Sales Person") + ":Link/Sales Person:150",
+ 
+        _("Domestic/Export") + ":Data:150"
+ 
+    ]
+ 
+ 
+def get_conditions(filters):
+ 
+    conditions = ""
+ 
+    if filters.get("creation_no"):
+ 
+        conditions += " AND so.name = %(creation_no)s"
+ 
+   
+ 
+    if filters.get("customer_name"):
+ 
+        filters["customer_name"] = f"%{filters['customer_name']}%"
+ 
+        conditions += " AND so.customer_name LIKE %(customer_name)s"
+ 
+    if filters.get("item_code"):
+ 
+        conditions += " AND soi.item_code = %(item_code)s"
+ 
+    if filters.get("currency"):
+ 
+        conditions += " AND so.currency = %(currency)s"
+ 
+    # DATE FILTER - FIXED (Using creation date)
+ 
+    if filters.get("from_date"):
+ 
+        conditions += " AND DATE(so.creation) >= %(from_date)s"
+ 
+    if filters.get("to_date"):
+ 
+        conditions += " AND DATE(so.creation) <= %(to_date)s"
+ 
+    return conditions
+ 
+ 
+def get_data(filters):
+ 
+    conditions = get_conditions(filters)
+ 
+    sql = f"""
+ 
+        SELECT
+ 
+            so.name AS creation_no,
+ 
+            DATE(so.creation) AS creation_date,
+ 
+            ROW_NUMBER() OVER (PARTITION BY so.name ORDER BY soi.idx) AS sr_no,
+ 
+            si.po_no AS po_no,
+ 
+            si.po_date AS po_date,
+ 
+            c.customer_code AS customer_code,
+ 
+            so.customer_name AS customer_name,
+ 
+            soi.party_item_code AS party_item_code,
+ 
+            soi.item_code AS item_code,
+ 
+            soi.item_name AS item_name,
+ 
+            soi.description AS description,
+ 
+            soi.qty AS po_qty,
+ 
+            soi.delivered_qty AS delivered_qty,
+ 
+            (soi.qty - soi.delivered_qty) AS open_qty,
+ 
+            soi.rate AS item_rate,
+ 
+            so.currency AS currency,
+ 
+            so.conversion_rate AS exchange_rate,
+ 
+           
+ 
+            (soi.base_amount) AS total_net_amount_inr,
+ 
+            (soi.delivered_qty * soi.base_rate) AS delivered_net_total_inr,
+ 
+            # (soi.base_amount * delivered_net_total_inr) AS balance_net_total_inr,
+         (IFNULL(soi.base_amount, 0) - (IFNULL(soi.delivered_qty, 0) * IFNULL(soi.base_rate, 0))) AS balance_net_total_inr,
+ 
+            so.delivery_date AS delivery_date,
+ 
+            (SELECT IFNULL(SUM(b.actual_qty), 0)
+ 
+                FROM `tabBin` b
+ 
+                WHERE b.item_code = soi.item_code
+ 
+            ) AS stock,
+ 
+            c.business_region_name AS business_region,
+ 
+            st.sales_person AS sales_person,
+ 
+            CASE
+ 
+                WHEN IFNULL(a.country, '') = 'India' THEN 'Domestic'
+ 
+                ELSE 'Export'
+ 
+            END AS domestic_export
+ 
+        FROM `tabSales Order` so
+ 
+        INNER JOIN `tabSales Order Item` soi ON soi.parent = so.name
+ 
+        LEFT JOIN `tabSales Team` st ON st.parent = so.name
+ 
+        LEFT JOIN `tabCustomer` c ON so.customer = c.name
+ 
+        LEFT JOIN `tabAddress` a ON a.name = so.customer_address
+ 
+        LEFT JOIN `tabSales Invoice Item` sii ON sii.so_detail = soi.name
+ 
+        LEFT JOIN `tabSales Invoice` si ON si.name = sii.parent
+ 
+    WHERE (so.status IS NULL OR so.status NOT IN ('Completed', 'To Bill'))
+    {conditions}
+ 
+        GROUP BY soi.name
+ 
+        ORDER BY so.creation ASC, so.name ASC
+ 
+    """
+ 
+    return frappe.db.sql(sql, filters, as_list=True)
+ 
+ 
