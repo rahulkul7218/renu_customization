ef51dcc4b9f8e535fee26b330e048e2d22a109c2
rahulkul7218 <kulkarnirahul134@gmail.com>
2025-11-28 15:27:02 +0530

    update lut no on sales invoice

--- renu_customization/patches/v_0/add_lut_no_from_and_to_date_on_sales_invoice.py
+++ renu_customization/patches/v_0/add_lut_no_from_and_to_date_on_sales_invoice.py
@@ -4,25 +4,19 @@ from frappe.custom.doctype.custom_field.custom_field import create_custom_field
 def execute():
     # LUT Number (Link)
     create_custom_field("Sales Invoice", {
-        "fieldname": "lut_number",
-        "label": "LUT Number",
+        "fieldname": "lut_no",
+        "label": "LUT No",
         "fieldtype": "Link",
         "options": "LUT Number",
         "insert_after": "amended_from"
     })
 
-    # From Date (Date)
-    create_custom_field("Sales Invoice", {
-        "fieldname": "lut_from_date",
-        "label": "From Date",
-        "fieldtype": "Date",
-        "insert_after": "lut_number"
-    })
+   
 
     # To Date (Date)
     create_custom_field("Sales Invoice", {
-        "fieldname": "lut_to_date",
-        "label": "To Date",
+        "fieldname": "lut_expiry_date",
+        "label": "LUT Expiry Date",
         "fieldtype": "Date",
         "insert_after": "lut_from_date"
     })
