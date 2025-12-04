54b4a4b6fd46fc463203c5402279ba92c27f92af
rahulkul7218 <kulkarnirahul134@gmail.com>
2025-12-04 10:35:28 +0530

    once record save then not update lut no and expiry date on sales invoice

--- renu_customization/public/js/fetched_data_lut_no_and_expiry_date_on_sales_invoice_from_gst_settings.js
+++ renu_customization/public/js/fetched_data_lut_no_and_expiry_date_on_sales_invoice_from_gst_settings.js
@@ -1,23 +1,58 @@
+// frappe.ui.form.on("Sales Invoice", {
+//     onload(frm) {
+//         frm.trigger("set_lut_values");
+//     },
+
+//     // refresh(frm) {
+//     //     frm.trigger("set_lut_values");
+//     // },
+
+//     company(frm) {
+//         frm.trigger("set_lut_values");
+//     },
+
+//     set_lut_values(frm) {
+//         if (!frm.doc.company) return;
+
+//         frappe.db.get_doc("GST Settings", frm.doc.company)
+//             .then(gst => {
+//                 frm.set_value("lut_no", gst.lut_no || "");
+//                 frm.set_value("lut_expiry_date", gst.lut_expiry_date || "");
+//             });
+//     }
+// });
+
+
 frappe.ui.form.on("Sales Invoice", {
     onload(frm) {
         frm.trigger("set_lut_values");
     },
-
-    // refresh(frm) {
-    //     frm.trigger("set_lut_values");
-    // },
 
     company(frm) {
         frm.trigger("set_lut_values");
     },
 
     set_lut_values(frm) {
+        // ❌ Do not update if document is saved/submitted
+        if (frm.doc.docstatus > 0) {
+            return;
+        }
+
+        // If company not selected, skip
         if (!frm.doc.company) return;
 
+        // Fetch LUT values from GST Settings
         frappe.db.get_doc("GST Settings", frm.doc.company)
             .then(gst => {
-                frm.set_value("lut_no", gst.lut_no || "");
-                frm.set_value("lut_expiry_date", gst.lut_expiry_date || "");
+                if (!gst) return;
+
+                // Set only if fields are empty (so manual values are not overwritten)
+                if (!frm.doc.lut_no) {
+                    frm.set_value("lut_no", gst.lut_no || "");
+                }
+                if (!frm.doc.lut_expiry_date) {
+                    frm.set_value("lut_expiry_date", gst.lut_expiry_date || "");
+                }
             });
     }
 });
