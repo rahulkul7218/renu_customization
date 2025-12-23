8ff9580f2ffe25ef334ef8464a73b3e0ede067cd
rahulkul7218 <kulkarnirahul134@gmail.com>
2025-12-23 12:04:00 +0530

    update

--- renu_customization/renu_customization/report/pending_sales_order_report/pending_sales_order_report.py
+++ renu_customization/renu_customization/report/pending_sales_order_report/pending_sales_order_report.py
@@ -365,7 +365,7 @@ def get_data(filters):
         LEFT JOIN `tabSales Invoice` si ON si.name = latest_sii.parent
         WHERE (so.status IS NULL OR so.status NOT IN ('Completed', 'To Bill', 'Cancelled'))
         # AND i.is_stock_item = 1
-        AND NOT (it.is_stock_item = 0 AND it.custom_is_freight_item = 1)
+        AND NOT (i.is_stock_item = 0 AND i.custom_is_freight_item = 1)
         AND (soi.qty - soi.delivered_qty) > 0
         # AND (so.amended_from IS NULL OR so.name = (
         #     SELECT MAX(name)
