04560420bab1bc69dc9b525ca916b1a78220b59a
rahulkul7218 <kulkarnirahul134@gmail.com>
2025-12-23 11:40:31 +0530

    rename is freight item

--- renu_customization/patches/v_0/add_is_service_item_checkbox_on_item.py
+++ renu_customization/patches/v_0/add_is_service_item_checkbox_on_item.py
@@ -3,13 +3,13 @@ import frappe
 def execute():
     if not frappe.db.exists("Custom Field", {
         "dt": "Item",
-        "fieldname": "custom_is_service_item"
+        "fieldname": "custom_is_freight_item"
     }):
         frappe.get_doc({
             "doctype": "Custom Field",
             "dt": "Item",
-            "label": "Is Service Item",
-            "fieldname": "custom_is_service_item",
+            "label": "Is Freight Item",
+            "fieldname": "custom_is_freight_item",
             "fieldtype": "Check",
             "insert_after": "is_stock_item",  # you can change position
             "default": 0
