3579cd2b43608a414471dae627aa7cbfbdc7b3fa
rahulkul7218 <kulkarnirahul134@gmail.com>
2025-12-26 17:49:35 +0530

    update in stock status report 3 columns

--- renu_customization/hooks.py
+++ renu_customization/hooks.py
@@ -244,6 +244,7 @@ app_license = "mit"
 
 after_migrate = [
     "renu_customization.patches.v_0.add_business_fields_to_item.execute",
+    
     "renu_customization.patches.v_0.addbusiness_region_field_on_customer.execute",
     "renu_customization.patches.v_0.add_supplier_code_field_on_supplier.execute",
     "renu_customization.patches.v_0.add_business_unit_field_on_sales_invoice_item.execute",
