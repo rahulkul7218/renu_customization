97656a1120d10f86cfaf93b3a8415436313841fe
rahulkul7218 <kulkarnirahul134@gmail.com>
2025-12-26 13:54:46 +0530

    add one condition if order type is e service domestic or e service export then and billing status is fully billed then that not show on pending sales order report

--- renu_customization/renu_customization/report/pending_sales_order_report/pending_sales_order_report.py
+++ renu_customization/renu_customization/report/pending_sales_order_report/pending_sales_order_report.py
@@ -364,6 +364,13 @@ def get_data(filters):
         ) latest_sii ON latest_sii.so_detail = soi.name
         LEFT JOIN `tabSales Invoice` si ON si.name = latest_sii.parent
         WHERE (so.status IS NULL OR so.status NOT IN ('Completed', 'To Bill', 'Cancelled'))
+        AND NOT (
+                so.invoice_type IN (
+                    'Engineering Service Domestic',
+                    'Engineering Service Export'
+                )
+                AND so.billing_status = 'Fully Billed'
+                )
         # AND i.is_stock_item = 1
         AND NOT (i.is_stock_item = 0 AND i.custom_is_freight_item = 1)
         AND (soi.qty - soi.delivered_qty) > 0
