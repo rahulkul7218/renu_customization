c990c0a5c38a7e44918faa34a403971c86e81a7f
rahulkul7218 <kulkarnirahul134@gmail.com>
2026-01-06 12:32:14 +0530

    update

--- renu_customization/hooks.py
+++ renu_customization/hooks.py
@@ -333,8 +333,8 @@ after_migrate = [
     "renu_customization.patches.v_0.add_delivery_term_field_in_so.execute",
     "renu_customization.patches.v_0.add_fields_in_si_docket_no.execute",
     "renu_customization.patches.v_0.add_port_of_loading_field_on_delivery_note.execute",
-    "renu_customization.patches.v_0.add_port_of_discharge_field_on_delivery_note.execute",
-    "renu_customization.patches.v_0.add_sales_order_on_delivery_note.execute"
+    "renu_customization.patches.v_0.add_port_of_discharge_field_on_delivery_note.execute"
+    
     
    #Naming series
     # "renu_customization.patches.v_0.create_document_naming_rule_on_salesinvoice_if_invoice_is_productdomestic.execute",
