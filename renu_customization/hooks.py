80e22b1e98ffbdfec67538a0870f922d2f8b2090
rahulkul7218 <kulkarnirahul134@gmail.com>
2026-01-06 11:54:14 +0530

    packing slip serial no comma seprated and add field port of loading

--- renu_customization/hooks.py
+++ renu_customization/hooks.py
@@ -332,6 +332,7 @@ after_migrate = [
     "renu_customization.patches.v_0.add_delivery_term_field_in_si.execute",
     "renu_customization.patches.v_0.add_delivery_term_field_in_so.execute",
     "renu_customization.patches.v_0.add_fields_in_si_docket_no.execute",
+    "renu_customization.patches.v_0.add_port_of_loading_field_on_delivery_note.execute"
     
    #Naming series
     # "renu_customization.patches.v_0.create_document_naming_rule_on_salesinvoice_if_invoice_is_productdomestic.execute",
---
+++ renu_customization/patches/v_0/add_port_of_loading_field_on_delivery_note.py
@@ -0,0 +1,27 @@
+import frappe
+
+def execute():
+    if not frappe.db.exists("DocType", "Delivery Note"):
+        return
+
+    # Check if field already exists
+    if frappe.db.exists("Custom Field", {
+        "dt": "Delivery Note",
+        "fieldname": "port_of_loading"
+    }):
+        return
+
+    frappe.get_doc({
+        "doctype": "Custom Field",
+        "dt": "Delivery Note",
+        "label": "Port of Loading",
+        "fieldname": "port_of_loading",
+        "default": "Mumbai",
+        "fieldtype": "Link",
+        "options": "Port of Loading",
+        "insert_after": "shipping_address_name",  # you can change position
+        "reqd": 0,
+        "read_only": 0
+    }).insert(ignore_permissions=True)
+
+    frappe.db.commit()
