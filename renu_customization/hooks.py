7156a4434b5c3cbc0308892139bfd68b60bd5ea5
rahulkul7218 <kulkarnirahul134@gmail.com>
2026-01-13 15:50:24 +0530

    update scheduler timing

--- renu_customization/hooks.py
+++ renu_customization/hooks.py
@@ -434,7 +434,7 @@ app_include_js = [
 
 scheduler_events = {
     "cron": {
-        "30 14 * * *": [
+        "0 0 * * *": [
             "renu_customization.renu_customization.page.mrp.mrp.auto_create_purchase_orders"
         ]
     }
