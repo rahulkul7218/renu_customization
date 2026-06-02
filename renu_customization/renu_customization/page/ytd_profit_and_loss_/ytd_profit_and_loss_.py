cdb9e178a8ba7a74c4dcde27f6225fe7ff7461bb
rahulkul7218 <kulkarnirahul134@gmail.com>
2026-06-02 09:56:05 +0530

    PYD

--- renu_customization/renu_customization/page/ytd_profit_and_loss_/ytd_profit_and_loss_.py
+++ renu_customization/renu_customization/page/ytd_profit_and_loss_/ytd_profit_and_loss_.py
@@ -260,6 +260,7 @@ def get_dashboard_data(company, filters=None):
 			
 		# Compute PYD to_date limit if previous fiscal year exists
 		pyd_to_date = None
+		prev_fy_end = None
 		if previous_fy:
 			prev_fy_start, prev_fy_end = get_fiscal_year_dates(company, previous_fy)
 			if current_fy_start <= today <= current_fy_end:
@@ -397,7 +398,8 @@ def get_dashboard_data(company, filters=None):
 		
 		# Receivables - Outstanding Amount
 		rec_ytd = get_outstanding_receivables(company, ytd_to_date)
-		rec_pyd = get_outstanding_receivables(company, pyd_to_date) if pyd_to_date else 0.0
+		# For PYD, use the previous fiscal year end date to get outstanding as of end of previous FY
+		rec_pyd = get_outstanding_receivables(company, prev_fy_end) if prev_fy_end else 0.0
 		rec_ytd_millions = convert_to_millions(rec_ytd)
 		rec_pyd_millions = convert_to_millions(rec_pyd)
 		rec_var_val = (rec_ytd_millions - rec_pyd_millions) * 1000000
@@ -405,7 +407,8 @@ def get_dashboard_data(company, filters=None):
 		
 		# Payables - Outstanding Amount
 		pay_ytd = get_outstanding_payables(company, ytd_to_date)
-		pay_pyd = get_outstanding_payables(company, pyd_to_date) if pyd_to_date else 0.0
+		# For PYD, use the previous fiscal year end date to get outstanding as of end of previous FY
+		pay_pyd = get_outstanding_payables(company, prev_fy_end) if prev_fy_end else 0.0
 		# Purchase total from Direct Expenses account
 		purchase_accounts = ['32 - DIRECT EXPENSES - RFAPL']
 		purchase_ytd = sum(get_account_balance_ytd(company, acc, current_fy, to_date=ytd_to_date) for acc in purchase_accounts)
